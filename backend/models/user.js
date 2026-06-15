f17331d74b5fc91a8a008c748bb846077e0129ab
Matheesha Koralegedara <matheeshakorale19@gmail.com>
2026-06-15 15:06:02 +0530

    Fixed Error

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, unique: true, sparse: true},
     email: { type: String, unique: true },
     password: String,
     role: { type: String, default: "user" }
