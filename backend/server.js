01a249567cb19036031f736221511a00206254c4
Matheesha Koralegedara <matheeshakorale19@gmail.com>
2026-06-15 12:10:02 +0530

    Add server setup and User Models

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/products', require('./routes/productRoutes'));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');    
+        app.listen(5000, () => console.log('Server running on port 5000'));
+    })
+    .catch(err => console.log(err));
